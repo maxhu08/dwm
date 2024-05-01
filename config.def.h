d58808a9cfd674086417ce0da92c60581b3da17d
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-01 18:40:37 -0500

    add colorscheme

--- config.def.h
+++ config.def.h
@@ -7,15 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Jetbrains Mono NerdFont:size=12", "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1e1e2e";
+static const char col_gray2[]       = "#6c7086";
+static const char col_gray3[]       = "#cdd6f4";
+static const char col_gray4[]       = "#1e1e2e";
+static const char col_accent[]      = "#f0abfc";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg         bg           border     */
+	[SchemeNorm] = { col_gray3, col_gray1 ,  col_gray2   },
+	[SchemeSel]  = { col_gray4, col_accent,  col_accent  },
 };
 
 /* tagging */
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accent, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
